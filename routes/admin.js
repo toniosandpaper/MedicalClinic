403ed92f9693fdbf5a16e9ae5aacb77c4bf1d7e2
toniosandpaper <toniozk04@gmail.com>
2026-03-25 01:32:20 -0500

    Update 2
    I have gotten the Home page working, I have inputted the gender, race, and ethnicity codes, and have the main skeleton for reports and inserting employees. I still need to get the insert to work correctly, the reports done, and to get the profile view to function, which it does not.

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+const id = 1;
 router.get('/profile', async (req, res) => {
-    res.render('admin/profile');
+    /*const fir = db.query("SELECT FirstName FROM employee WHERE employeeID="+id);
+    const las = db.query("SELECT LastName FROM employee WHERE employeeID="+id);
+    const bd = db.query("SELECT Birthdate FROM employee WHERE employeeID="+id);
+    const gen = db.query("SELECT GenderText FROM employee,gender WHERE employeeID="+id+" AND employee.GenderCode=gender.GenderCode");
+    const rac = db.query("SELECT RaceText FROM employee,race WHERE employeeID="+id+" AND employee.RaceCode=race.RaceCode");
+    const eth = db.query("SELECT EthnicityText FROM employee,ethnicity WHERE employeeID="+id+" AND employee.EthnicityCode=ethnicity.EthnicityCode");
+    const addr = db.query("SELECT FirstName FROM employee WHERE employeeID="+id);
+    const pho = db.query("SELECT FirstName FROM employee WHERE employeeID="+id);
+    const dep = db.query("SELECT DepartmentName FROM employee,department WHERE employeeID="+id+" AND employee.DepartmentID=department.DepartmentID");
+    const rol = db.query("SELECT FirstName FROM employee WHERE employeeID="+id);*/
+    res.render('admin/profile');//,{{First = fir},{Last = las},{Birth = bd},{Gender = gen},{Race = rac},{Ethnic = eth},{Address = addr},{Phone = pho},{Depart = dep},{Role = rol}});
 });
 router.get('/home', async (req, res) => {
     res.render('admin/home');
@@ -11,15 +22,22 @@ router.get('/home', async (req, res) => {
 router.get('/adde', async (req, res) => {
     res.render('admin/adde');
 });
-
 router.get('/', async (req, res ) =>{
     const [rows] = await db.query("SELECT * FROM employee WHERE Role='Admin'")
     res.json(rows);
 })
-
+router.get('/repgar', async (req, res) => {
+    res.render('admin/repgar');
+})
+router.get('/repgrr', async (req, res) => {
+    res.render('admin/repgrr');
+})
+router.get('/repdar', async (req, res) => {
+    res.render('admin/repdar');
+})
 // Creating and Employee - POST
 router.post('/addemp', (req, res) => {
-    const q = "INSERT INTO employee (`FirstName`,`LastName`,`Birthdate`,`GenderCode`,`RaceCode`,`EthnicityCode`,`Role`,`Address`,`PhoneNumber`,`Email`,`Password`,`DepartmentID`) VALUES (?)"
+    const q = "INSERT INTO employee (`FirstName`,`LastName`,`Birthdate`,`GenderCode`,`RaceCode`,`EthnicityCode`,`Role`,`Address`,`PhoneNumber`,`Email`,`Password`,`DepartmentID`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
     //const q = "INSERT INTO employee (`FirstName`,`DepartmentID`) VALUES (?)";
     const r = [
         req.body.FirstName,
